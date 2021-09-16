2f2e7ab4902b6b7f064761df7c39de115b0cda2c
Alexandr <77400439+Alexandr-R-94@users.noreply.github.com>
2021-09-16 15:10:34 +0200

    IFrame  эксперимент #8

--- src/test/java/steps/PopUpWindowStep.java
+++ src/test/java/steps/PopUpWindowStep.java
@@ -2,7 +2,9 @@ package steps;
 
 import baseEntities.BaseStep;
 import core.BrowsersService;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import pages.ProjectPage;
 
@@ -13,15 +15,16 @@ public class PopUpWindowStep extends BaseStep {
 
     public void popUpWindow() throws InterruptedException {
         JavascriptExecutor js = (JavascriptExecutor) browsersService.getDriver();
+
         ProjectPage projectPage = new ProjectPage(browsersService, false);
         logger.info("Нажатие на кнопку уведомлений");
         js.executeScript("arguments[0].click();", projectPage.bellBtn);
         //projectPage.bellButton();
         logger.info("Переход в iframe через id");
         browsersService.getDriver().switchTo().frame(0);
-
+        WebElement element = browsersService.getDriver().findElement(By.xpath("//h3[.='Latest changes']"));
         logger.info("Сравнение ожидаемого текста с фактической");
-       // Assert.assertTrue(projectPage.iFrameText.isDisplayed());
-        js.executeScript("arguments[0].scrollIntoView(true);", projectPage.iFrameText);
+        Assert.assertTrue(element.isDisplayed());
+        //js.executeScript("arguments[0].scrollIntoView(true);", element);
     }
 }
